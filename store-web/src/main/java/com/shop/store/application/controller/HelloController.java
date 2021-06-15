f11c7809bfa5da476b09c195fa785ccbefc00564
lk-j <381584494@qq.com>
2021-06-15 19:25:54 +0800

    add code

--- store-web/src/main/java/com/shop/store/application/controller/HelloController.java
+++ store-web/src/main/java/com/shop/store/application/controller/HelloController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String getHello() {
+    @GetMapping("/user/info")
+    public String getUserInfo() {
+
         return "hello";
     }
 
